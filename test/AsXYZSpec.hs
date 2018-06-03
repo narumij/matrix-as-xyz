8f74621463d70b8ba2024320943a943963546c13
narumij <narumij@me.com>
2018-06-04 02:53:55 +0900

    test

--- test/AsXYZSpec.hs
+++ test/AsXYZSpec.hs
@@ -1,22 +1,19 @@
 module AsXYZSpec where
 
-import Test.Hspec
-import Test.Hspec.QuickCheck
-import Test.QuickCheck hiding ((.&.))
+import Control.Exception (evaluate)
 
+import Test.Hspec
 import Data.Ratio
 import Data.Matrix
 import Data.Matrix.AsXYZ
 
 spec :: Spec
 spec = do
-  return ()
-  -- describe "AsXYZ" $ do
-  --   prop "reverse test Matrix Int" $ \a b c d e f g h i j k l->
-  --     let m = fromList 4 4 [a,b,c,d, e,f,g,h, i,j,k,l, 0,0,0,1] :: Matrix Int
-  --     in (fromXYZ $ prettyXYZ m) `shouldBe` m
 
-  -- describe "AsXYZ" $ do
-  --   prop "reverse test Matrix Rational" $ \a b c d e f g h i j k l->
-  --     let m = fromList 4 4 [a,b,c,d, e,f,g,h, i,j,k,l, 0,0,0,1] :: Matrix Rational
-  --     in (fromXYZ $ prettyXYZ m) `shouldBe` m
+   describe "Data.Matrix.AsXYZ.fromXYZ" $ do
+
+     it "read empty throws exception" $ do
+       evaluate (fromXYZ "") `shouldThrow` anyException
+
+     it "read x,y,z" $ do
+       fromXYZ "x,y,z" `shouldBe` (identity 4)
