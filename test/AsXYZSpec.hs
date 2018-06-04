0a57859c18e0b17195c352c938822654330a3fd3
narumij <narumij@me.com>
2018-06-05 01:26:57 +0900

    test

--- test/AsXYZSpec.hs
+++ test/AsXYZSpec.hs
@@ -17,3 +17,38 @@ spec = do
 
      it "read x,y,z" $ do
        fromXYZ "x,y,z" `shouldBe` (identity 4)
+
+
+   describe "Data.Matrix.AsXYZ.prettyXYZ" $ do
+
+     it "show 0 (3x3)" $ do
+       prettyXYZ (zero 3 3) `shouldBe` "0,0,0"
+
+     it "show 0 (3x4)" $ do
+       prettyXYZ (zero 3 4) `shouldBe` "0,0,0"
+
+     it "show 0 (4x4)" $ do
+       prettyXYZ (zero 4 4) `shouldBe` "0,0,0"
+
+     it "show 1 (4x4)" $ do
+       prettyXYZ (identity 4) `shouldBe` "x,y,z"
+
+     it "show 1 (3x4)" $ do
+       prettyXYZ (submatrix 1 3 1 4 $ identity 4) `shouldBe` "x,y,z"
+
+     it "show 1 (3x3)" $ do
+       prettyXYZ (submatrix 1 3 1 3 $ identity 4) `shouldBe` "x,y,z"
+
+     it "positive first" $ do
+       prettyXYZ (fromLists [[1,-1,-1],[-1,1,-1],[-1,-1,1]]) `shouldBe` "x-y-z,y-x-z,z-x-y"
+
+     it "number last" $ do
+       prettyXYZ (fromLists [[-1,-1,-1,-1],[-1,-1,-1,0],[-1,-1,-1,1]]) `shouldBe` "-x-y-z-1,-x-y-z,-x-y-z+1"
+
+   describe "Data.Matrix.AsXYZ.prettyABC" $ do
+
+     it "show 0" $ do
+       prettyABC (zero 4 4) `shouldBe` "0,0,0"
+
+     it "show 1" $ do
+       prettyABC (identity 4) `shouldBe` "a,b,c"
