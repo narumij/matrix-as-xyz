bc278e98ccb3a43f86f25c9b88898dabe7fe9812
narumij <narumij@users.noreply.github.com>
2020-07-21 13:08:35 +0900

    version 0.1.1.2

--- test/doctests.hs
+++ test/doctests.hs
@@ -1,6 +1,10 @@
+module Main (main) where
+
 import Test.DocTest
 
+main :: IO ()
 main = doctest [
+  "--fast",
   "src/Data/Ratio/Slash.hs",
   "src/Data/Ratio/ParseFloat.hs",
   "src/Data/Matrix/AsXYZ.hs"
