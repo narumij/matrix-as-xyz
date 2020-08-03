6d515e429a326805b10adaba17440b3a97757213
narumij <narumij@me.com>
2020-08-03 18:33:37 +0900

    refactoring

--- src/Data/Matrix/AsXY/Parse.hs
+++ src/Data/Matrix/AsXY/Parse.hs
@@ -7,13 +7,12 @@ Stability   : experimental
 Portability : ?
 -}
 module Data.Matrix.AsXY.Parse (
-  P.Value,
   equivalentPositions,
   transformPpABC,
   transformQqXYZ,
-  P.ratio,
-  P.integral,
-  P.floating,
+  ratio,
+  integral,
+  floating,
   ) where
 
 import Control.Monad
@@ -21,7 +20,7 @@ import Data.Char
 import Data.Maybe
 import Data.List
 import Text.ParserCombinators.Parsec
-import qualified Data.Matrix.AsXYZ.Parse as P
+import Data.Matrix.AsXYZ.Parse (ReadNum(..),Var(..),overlap,sign,elementBody,minus,ratio,integral,floating)
 
 xy :: CharParser () Char
 xy = oneOf "xyXY"
@@ -31,61 +30,61 @@ ab = oneOf "abAB"
 
 -- | General equivalent positions parser
 equivalentPositions :: Num a =>
-　　　　　　　　　　　　　　P.ReadNum a -- ^ use converter below
+　　　　　　　　　　　　　　ReadNum a -- ^ use converter below
 　　　　　　　　　　　　 -> CharParser () [[a]]
 equivalentPositions = components xy
 
 -- | Same as equivalentPositions but uses abc instead of xyz
-transformPpABC :: Num a => P.ReadNum a -> CharParser () [[a]]
+transformPpABC :: Num a => ReadNum a -> CharParser () [[a]]
 transformPpABC = components ab
 
 -- | Alias of equivalentPositions
-transformQqXYZ :: Num a => P.ReadNum a -> CharParser () [[a]]
+transformQqXYZ :: Num a => ReadNum a -> CharParser () [[a]]
 transformQqXYZ = components xy
 
 v c = f $ toLower <$> c
   where
-    f (Just 'x') = P.X
-    f (Just 'a') = P.X
-    f (Just 'y') = P.Y
-    f (Just 'b') = P.Y
-    f Nothing = P.Z
+    f (Just 'x') = X
+    f (Just 'a') = X
+    f (Just 'y') = Y
+    f (Just 'b') = Y
+    f Nothing = Z
 
-one :: Num a => CharParser () Char -> P.ReadNum a -> CharParser () (P.Var a)
+one :: Num a => CharParser () Char -> ReadNum a -> CharParser () (Var a)
 one var numRead = do
-  s <- optionMaybe P.sign
+  s <- optionMaybe sign
   option () spaces
-  (n,l) <- P.elementBody var numRead
-  return $ v l . P.minus s . fromMaybe 1 $ n
+  (n,l) <- elementBody var numRead
+  return $ v l . minus s . fromMaybe 1 $ n
 
-other :: Num a => CharParser () Char -> P.ReadNum a -> CharParser () (P.Var a)
+other :: Num a => CharParser () Char -> ReadNum a -> CharParser () (Var a)
 other var numRead = do
-  s <- P.sign
+  s <- sign
   option () spaces
-  (n,l) <- P.elementBody var numRead
-  return $ v l . P.minus (Just s) . fromMaybe 1 $ n
+  (n,l) <- elementBody var numRead
+  return $ v l . minus (Just s) . fromMaybe 1 $ n
 
-constructRow :: Num a => [P.Var a] -> [a]
+constructRow :: Num a => [Var a] -> [a]
 constructRow = map (fromMaybe 0 . listToMaybe . catMaybes) . transpose . map toArray
   where
-    toArray (P.X n) = [Just n,Nothing,Nothing]
-    toArray (P.Y n) = [Nothing,Just n,Nothing]
-    toArray (P.Z n) = [Nothing,Nothing,Just n]
+    toArray (X n) = [Just n,Nothing,Nothing]
+    toArray (Y n) = [Nothing,Just n,Nothing]
+    toArray (Z n) = [Nothing,Nothing,Just n]
 
-component :: Num b => CharParser () Char -> P.ReadNum b -> CharParser () [b]
+component :: Num b => CharParser () Char -> ReadNum b -> CharParser () [b]
 component var numRead = do
   option () spaces
   x <- one var numRead
   xs <- many (other var numRead)
   option () spaces
   let mm = x : xs
-  if P.overlap (map void mm)
+  if overlap (map void mm)
     then
       fail "overlaps var type"
     else
       return (constructRow mm)
 
-components :: Num a => CharParser () Char -> P.ReadNum a -> CharParser () [[a]]
+components :: Num a => CharParser () Char -> ReadNum a -> CharParser () [[a]]
 components var conv = do
   a <- component var conv
   char ','
